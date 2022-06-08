31d079d5f59ca26f3b5c1d928b1c0f94f3a3d17c
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 15:02:34 -0700

    Return absolute val

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - This function prints the
+ * absolute value of any interger entered.
+ *
+ * @int: The number to be checked.
+ *
+ * Return: returns 0 on succes.
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
