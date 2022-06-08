2a0f4fefbc0006d0471ec0937da1c3fe82fd25fc
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 02:11:46 -0700

    putchar _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: This program prints _putchar,
+ * followed by a new line.
+ *
+ * Return: returns 0 on success.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
