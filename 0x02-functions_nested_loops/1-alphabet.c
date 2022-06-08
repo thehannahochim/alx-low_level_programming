4dcead86ea55ff0e44e22f82adb0897089bf2608
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 02:47:46 -0700

    Function lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: This function prints the alphabet in lowercase.
+ *
+ * Return: returns 0 on success.
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
