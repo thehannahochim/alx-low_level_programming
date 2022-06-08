1b186e3252a217fd2dd7b800fd2b9272efcbf49b
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 13:33:08 -0700

    Check isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _isalpha() - This function checks
+ * for alphabetic character.
+ *
+ * @c: This is the input to be checked.
+ *
+ * Return: returns 1 on success and 0 if otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
