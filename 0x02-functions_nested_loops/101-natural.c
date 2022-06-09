ccfb620e533906e7e1ff35d21ec0a2741fad7d2e
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 19:44:36 -0700

    Times table v2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 /**
- * main - Entry point
+ * main - sums multiples of 3 or 5
  *
- * Return: return 0 on success.
+ * Description: multiples between 0 and 1024
+ *
+ * Return: Always(0) success
  */
-
 int main(void)
 {
-	int n;
-	if (n % 3 = 0 || n % 5 = 0)
+	int start_num, end_num, total;
+
+	end_num = 1024;
+	total = 0;
+
+	for (start_num = 0; start_num < end_num; start_num++)
 	{
-		sum n;
+		if ((start_num % 3 == 0) || (start_num % 5 == 0))
+		{
+			total = total + start_num;
+		}
+		else
+		{
+			continue;
+		}
 	}
+	printf("%d", total);
+	printf("\n");
+
+	return (0);
 }
