696f169cc4d0597b8e233668ffda38b55fc2dd91
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 17:19:59 -0700

    Add them up!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Function adds two integers
+ *
+ * @n1: First integer
+ * @n2: Second integer
+ *
+ * Return: result
+ */
+
+int add(int n1, int n2)
+{
+	int result;
+	result = n1 + n2;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * times_table - This program prints
+ * the 9 times table, stating with 0.
+ *
+ * Return: times table.
+ */
+
+void times_table(void)
+{
+
