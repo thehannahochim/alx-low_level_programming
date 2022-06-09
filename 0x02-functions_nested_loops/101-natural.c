6a9e48c86df5f02162f754ed55d368796ae66bd0
Hannah Ochim <hannah099099@gmail.com>
2022-06-08 19:13:46 -0700

    Times Table +

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0 on success.
+ */
+
+int main(void)
+{
+	int n;
+	if (n % 3 = 0 || n % 5 = 0)
+	{
+		sum n;
+	}
+}
