01161edcdc04cd1d8f4463d8ebf3508f1d9ccf4e
Hannah Ochim <hannah099099@gmail.com>
2022-06-22 18:39:33 -0700

    recursion

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - This function swaps two integers.
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n, m;
+
+	a = &n;
+	b = &m;
+	*a = m;
+	*b = n;
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt_rec(int n, int x);
+int is_prime_number(int n);
+int is_prime(int n, int x);
+int is_palindrome(char *s);
+int palindrome(char *s, int first, int last);
+int wildcmp(char *s1, char *s2);
+
+#endif
