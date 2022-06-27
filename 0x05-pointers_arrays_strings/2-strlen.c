c8fdfca330ee944db075b6beaa4767a963fa41f1
Hannah Ochim <hannah099099@gmail.com>
2022-06-26 17:55:44 -0700

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,17 +6,20 @@
  * Description: This function prints the
  * length of a string.
  *
+ * @s: String to be counted.
+ *
  * Return: retuns length of string.
  */
 
 int _strlen(char *s)
 {
 	int length = 0;
-	int i=0;
+	int i = 0;
 
-	for (s[i] = 0;s[i] <= '\0'; s[i]++)
+	while (s[i] != '\0')
 	{
 		length = length + 1;
+		i++;
 	}
 	return(length);
 }
