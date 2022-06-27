100bb8796881da035c506807fa2e5fa705de12d2
Hannah Ochim <hannah099099@gmail.com>
2022-06-26 18:21:18 -0700

    _putchar string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - This function is designed
+ * to print out a string without
+ * the use of standard library functions.
+ *
+ * @str: Pointer to the string.
+ *
+ * return: returns void.
+ */
+
+void _puts(char *str)
+{
+	int i=0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
